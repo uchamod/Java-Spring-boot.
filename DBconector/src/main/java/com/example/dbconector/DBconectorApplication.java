f6c52ec8c4df4346d134ab415345df2574cff53f
Chamod Udara <128582580+uchamod@users.noreply.github.com>
2024-02-10 01:33:13 +0530

    Add files via upload
    This is a one of ways that connect mysql server to spring boot application.Here i used spring data JPA & MYSQL driver dependencies.
    And i used MYSQL workbench to create database.

---
+++ DBconector/src/main/java/com/example/dbconector/DBconectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbconector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBconectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBconectorApplication.class, args);
+	}
+
+}
---
+++ DBconector/src/test/java/com/example/dbconector/DBconectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbconector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBconectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
